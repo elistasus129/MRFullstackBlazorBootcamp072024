b91d1435633343c6269746cfe06fdc9eb41d43a2
Mert Ramazanoglu <61631884+elistasus129@users.noreply.github.com>
2024-07-10 22:22:43 +0300

    d

---
+++ MR.BlazorWasmStarter/Enums/CheckBoxActionType.cs
@@ -0,0 +1,10 @@
+﻿namespace MR.BlazorWasmStarter.Enums
+{
+    public enum CheckBoxActionType
+    {
+        Numbers=1,
+        Lowercase=2,
+        Uppercase=3,
+        SpecialChars=4
+    }
+}
